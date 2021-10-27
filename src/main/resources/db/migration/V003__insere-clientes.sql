81b27d2b94f00406d30879661b47298e0eceaee0
gustavojesus <gustavohenriqueaj@gmail.com>
2021-10-27 19:55:06 -0300

    CRUD - cliente

---
+++ src/main/resources/db/migration/V003__insere-clientes.sql
@@ -0,0 +1,3 @@
+insert into cliente (nome, email, fone) values ("Joao", "joaodascoves@email.com", "11000004444");
+insert into cliente (nome, email, fone) values ("Maria", "mariacachos@email.com", "11000008888");
+insert into cliente (nome, email, fone) values ("Chico", "chico@email.com", "11000009999");
